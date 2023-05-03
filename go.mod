815375ae6279ae2caf7111f034b5f2c1d76a2c27
Farhan Altariq <farhanaaxy@gmail.com>
2023-05-03 14:25:32 +0700

    init wa bot project

---
+++ go.mod
@@ -0,0 +1,18 @@
+module hanz/ai
+
+go 1.20
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/Rhymen/go-whatsapp v0.1.1 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/mdp/qrterminal/v3 v3.0.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	go.mau.fi/libsignal v0.1.0 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20230427180258-7f679583b39b // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+
+	// sqlite3 connector
+	_ "github.com/mattn/go-sqlite3"
+
+	// qrencode for QR code rendering
+	"github.com/mdp/qrterminal/v3"
+)
+
+func eventHandler(client *whatsmeow.Client, evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		// if not from me and not empty
+		msg := v.Message.ExtendedTextMessage.GetText()
+		sender := v.Info.MessageSource.Chat
+		senderName := v.Info.PushName
+		if v.Info.IsFromMe || msg == "" {
+			return
+		}
+
+		fmt.Println(senderName, " | ", sender)
+		fmt.Println("Message : ", msg)
+
+		// reply message
+		protoMsg := &proto.Message{
+			ExtendedTextMessage: &proto.ExtendedTextMessage{
+				// text to be send to sender
+				Text: &msg,
+			},
+		}
+		client.SendMessage(context.Background(), sender, protoMsg)
+	}
+}
+
+func connect(client *whatsmeow.Client) error {
+	var err error
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				// Render the QR code here
+				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
+				fmt.Println("QR code:", evt.Code)
+
+				// render the QR code and wait for the user to scan it
+				// then continue
+				config := qrterminal.Config{
+					Level:     qrterminal.L,
+					Writer:    os.Stdout,
+					BlackChar: qrterminal.WHITE,
+					WhiteChar: qrterminal.BLACK,
+					QuietZone: 1,
+				}
+				qrterminal.GenerateWithConfig(evt.Code, config)
+				fmt.Println("Scan the QR code above")
+
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	dbLog := waLog.Stdout("Database", "DEBUG", false)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client.AddEventHandler(func(evt interface{}) {
+		eventHandler(client, evt)
+	})
+
+	err = connect(client)
+	if err != nil {
+		panic(err)
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
